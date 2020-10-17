959a498ff5f32409b9c667cf50df573d0e080453
chendelin1982 <chendelin1982@126.com>
2020-10-17 11:04:27 +0800

    vue

---
+++ gridsome.config.js
@@ -0,0 +1,19 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: '网久软件',
+  siteDescription: '长沙网久软件有限公司（Websoft9）是专业的云镜像部署和服务提供商，致力在云端简化Web应用软件的安装和部署，业已对国内外200+个高质量的应用软件、建站软件、基础软件和开发者工具进行了整理、归类、性能优化，并以一键式镜像安装包的方式发布到阿里云、腾讯云、百度云、华为企业云等，任何客户可以通过弹性的方式一键获取、快速安装。',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+      },
+    },
+  ],
+}
---
+++ src/layouts/Default.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <g-link to="/"><g-image src="~/images/logo.png" to="/" width="150"/></g-link>
+      <nav class="nav">
+        <g-link class="nav__link" to="/apps/">应用</g-link>
+        <g-link class="nav__link" to="/apps/">价格</g-link>
+        <g-link class="nav__link" to="https://support.websoft9.com">支持</g-link>
+        <g-link class="nav__link" to="https://blog.websoft9.com">博客</g-link>
+        <g-link class="nav__link" to="/apps/">伙伴</g-link>
+        <g-link class="nav__link" to="/about/">资源</g-link>
+      </nav>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
