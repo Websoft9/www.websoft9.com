de32e999bbdb48601aa183ddc8d3597739fe9679
赵璟 <jing.zhao@websoft9.com>
2024-01-15 15:48:37 +0800

    Update update_contentful.py

--- contentful/update_contentful.py
+++ contentful/update_contentful.py
@@ -41,7 +41,12 @@ def update_entries(content_type_id, fields_to_update, update_conditions=None):
         query = {'content_type': content_type_id}
         if update_conditions:
             for field, value in update_conditions.items():
-                query[f'fields.{field}'] = value
+                if value == 'true':
+                    query[f'fields.{field}'] = True
+                elif value == 'false':
+                    query[f'fields.{field}'] = False
+                else:
+                    query[f'fields.{field}'] = value
 
         # 根据条件查找条目
         entries_to_update = fetch_all_entries(environment, query)
