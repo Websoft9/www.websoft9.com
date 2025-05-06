63a698a7400a56fbd9c68d811d6f3fd52ebee348
qiaofeng1227 <27513732@qq.com>
2025-05-06 15:36:38 +0800

    add trial request page

--- source/src/pages/trialrequest.jsx
+++ source/src/pages/trialrequest.jsx
@@ -17,7 +17,7 @@ const TrialRequestPage = ({ pageContext, location, data }) => {
             <main className="site-wrapper-reveal">
                 <PageHeader data={data.allContentfulPage.nodes[0].content[0]} />
 
-                <IframeArea data={data.allContentfulPage.nodes[0].content[1]?.link?.[0].value} width="100%" height="1500px" />
+                <IframeArea data={data.allContentfulPage.nodes[0].content[1]?.link?.[0].value} width="100%" height="600px" />
 
                 <CtaArea data={data.allContentfulPage.nodes[0].content[2]} />
 
